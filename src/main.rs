c30f7260ca9906365b626fb57e216f876029724e
hejian <9618058+xibufengqing@user.noreply.gitee.com>
2022-06-01 12:27:31 +0800

    clear coder

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
-use std::fs;
-
-mod fibloop;
-mod scap;
-mod question;
-
 fn main() {
-    let url = "https://www.rust-lang.org";
-    let _output ="rust.md";
-
-    println!("Fetching url : {}" ,url);
-
-    let _body = reqwest::blocking::get(url).unwrap().text().unwrap();
-
-    println!("Converting html to markdown...");
-
-    let md = html2md::parse_html(&_body);
-    fs::write(_output,md.as_bytes()).unwrap();
-
-    println!("Converted markdown has been saved in {}." ,_output);
+  
 
     
 }
--- target/debug/deps/scrape_url-e7b779d9adb9000b.d
+++ target/debug/deps/scrape_url-e7b779d9adb9000b.d
@@ -1,7 +1,5 @@
-/Users/hejian/Desktop/work_project/rust_project/scrape_url/target/debug/deps/scrape_url-e7b779d9adb9000b: src/main.rs src/fibloop.rs src/scap.rs
+/Users/hejian/Desktop/work_project/rust_project/scrape_url/target/debug/deps/scrape_url-e7b779d9adb9000b: src/main.rs
 
-/Users/hejian/Desktop/work_project/rust_project/scrape_url/target/debug/deps/scrape_url-e7b779d9adb9000b.d: src/main.rs src/fibloop.rs src/scap.rs
+/Users/hejian/Desktop/work_project/rust_project/scrape_url/target/debug/deps/scrape_url-e7b779d9adb9000b.d: src/main.rs
 
 src/main.rs:
-src/fibloop.rs:
-src/scap.rs:
