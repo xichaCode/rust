aeee9ee9063c93a3490b2765f34b045e1e7c55d8
hejian <9618058+xibufengqing@user.noreply.gitee.com>
2022-06-03 23:10:26 +0800

    add struce

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::collections::BTreeMap;
 fn main() {
-  
+    let mut map = BTreeMap::new();
+    map.insert("hello", "world");
+    print!("map: {:?}", map);
 
-    
+    iter_fnu();
 }
 
+fn iter_fnu(){
+    let numbers = vec![1,2,3,4,5,6,7,8,9];
+
+    let even_number: Vec<_> = numbers
+    .into_iter()
+    .filter(|n| n % 2 == 0)
+    .collect();
+    println!("{:?}", even_number);
+}
