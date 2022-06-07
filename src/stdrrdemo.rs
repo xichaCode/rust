1626c1d4ac2aea1364628be48dd204a68bf18e21
hejian <9618058+xibufengqing@user.noreply.gitee.com>
2022-06-07 14:40:51 +0800

    trait

--- src/stdrrdemo.rs
+++ src/stdrrdemo.rs
@@ -18,7 +18,7 @@ pub trait Free {
     fn feature2(&self);
 }
 
-pub trait Personal: Free {
+pub trait Personal : Free {
     fn advance_feature(&self);
 }
 
@@ -42,6 +42,7 @@ pub struct FreePlan;
 pub struct PersonalPlan(f32);
 
 impl <T> Customer<T> {
+    //构造函数，返回Customer的一个实例
     pub fn new(name: String) -> Self{
         Self{
             id: NEXT_ID.fetch_add(1, Ordering::Relaxed),
@@ -57,6 +58,12 @@ impl From<Customer<FreePlan>> for Customer<PersonalPlan> {
     }
 }
 
+impl Personal for Customer<FreePlan> {
+    fn advance_feature(&self){
+        println!("hejian ++++++++++++advance_feature,{}",self.name);
+    }
+}
+
 pub fn subscribe(customer: Customer<FreePlan>, payment: f32) -> Customer<PersonalPlan>{
     let _plan = PersonalPlan(payment);
     customer.into()
@@ -67,9 +74,19 @@ mod tests {
     use super::*;
     #[test]
     fn test_customer(){
-        let customer = Customer::<FreePlan>::new("hejina".into());
+        //创建Customer 实例，其中new相当于java的构造函数, 实例为customer的结构体, 结构体类型为FreePlan
+        //结构体实体实现了Free tarit ，其中有两个方法，feature1, feature2
+        let customer = Customer::<FreePlan>::new("hejina".into()); 
+        //通过customer实例，访问结构体中的方法 feature1 和 feature2
         customer.feature1();
         customer.feature2();
+        //泛型FreePlan的customer实例，实现PersonalPlan trait，即advance_feature 
+        customer.advance_feature();
+        //创建泛型为PersonlPlan的customer 实例，其中new相当于java的构造函数, 实例为Customer的结构体, 结构体类型为PersonlPlan
+        //PersonalPlan是一个结构构类型，实现了Personal tarit
+        let customer_ = Customer::<PersonalPlan>::new("hejian_".into());
+        customer_.advance_feature();
+    
         let customer = subscribe(customer, 6.99);
         customer.feature1();
         customer.feature2();
