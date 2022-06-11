b90005592852dba80da34f43636f148b5a96e553
hejian <9618058+xibufengqing@user.noreply.gitee.com>
2022-06-11 18:23:08 +0800

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,6 @@
-use nom::character::is_digit;
-use nom::byte::streaming::take_while;
-use nom::IResult;
-
 fn main() {
     
     
 }
 
-fn jud_digit(char: char) -> bool {
-    is_digit(chr as u8)
-}
 
-#[test]
-fn test_split_digit() {
-    let s = "123abc";
-    let r: IResult<&str, &str> = take_while(jud_digit)(s);
-
-    let remain_expanded = "abc";
-    let digit_expected ="123";
-    assert_eq!(Ok((remain_expanded, digit_expected)), r)ß
-}
