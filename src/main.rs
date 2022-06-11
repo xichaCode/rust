c422754465a4078191ab69315b5da5b3fa6a80d1
hejian <9618058+xibufengqing@user.noreply.gitee.com>
2022-06-11 18:49:20 +0800

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,65 @@
 use std::marker::PhantomData;
-use supper::*;
+
 #[derive(Debug, Default)]
 pub struct Equation<IterMethod> {
     current: u32,
-    _methods: PhantomData<IterMethod>,
+    _method: PhantomData<IterMethod>, 
 }
+
+// 线性增长
 #[derive(Debug, Default)]
 pub struct Linear;
 
+// 二次增长
 #[derive(Debug, Default)]
 pub struct Quadratic;
 
 impl Iterator for Equation<Linear> {
     type Item = u32;
+
     fn next(&mut self) -> Option<Self::Item> {
         self.current += 1;
         if self.current >= u32::MAX {
             return None;
         }
+
         Some(self.current)
     }
 }
 
 impl Iterator for Equation<Quadratic> {
     type Item = u32;
+
     fn next(&mut self) -> Option<Self::Item> {
         self.current += 1;
-        if self.current >= u16::MAX as  u32 {
+        if self.current >= u32::MAX {
             return None;
         }
-        Some((self.current * self.current))
+
+        Some(self.current * self.current)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn test_linear(){
-        let mut equation = Equation::<Linear>::Default();
-        assert_eq!(Some(1),equation.next());
-        assert_eq!(Some(2),equation.next());
-        assert_eq!(Some(3),equation.next());
+    fn test_linear() {
+        let mut equation = Equation::<Linear>::default();
+        assert_eq!(Some(1), equation.next());
+        assert_eq!(Some(2), equation.next());
+        assert_eq!(Some(3), equation.next());
     }
 
     #[test]
-    fn test_quadratic(){
-        let mut equation = Equation::<Quadratic>::Default();
-        assert_eq!(Some(1),equation.next());
-        assert_eq!(Some(4),equation.next());
-        assert_eq!(Some(9),equation.next());
+    fn test_quadratic() {
+        let mut equation = Equation::<Quadratic>::default();
+        assert_eq!(Some(1), equation.next());
+        assert_eq!(Some(4), equation.next());
+        assert_eq!(Some(9), equation.next());
     }
 }
 fn main() {
-    
-    
+
 }
-
-
